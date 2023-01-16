c3a0d9296fcc20882d5525069e62fade158e4b5e
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-16 10:00:12 -0800

    removing native base, fix for dependencies

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
-import { NativeBaseProvider } from "native-base";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import NewsScreen from "./src/screens/NewsScreen";
@@ -12,31 +11,29 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName;
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
 
-              if (route.name === "Home") {
-                iconName = "home";
-              } else if (route.name === "News") {
-                iconName = "book-open";
-              } else if (route.name === "Weather") {
-                iconName = "cloud";
-              }
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "News") {
+              iconName = "book-open";
+            } else if (route.name === "Weather") {
+              iconName = "cloud";
+            }
 
-              return <Feather name={iconName} size={size} color={color} />;
-            },
-            activeTintColor: "tomato",
-            inactiveTintColor: "gray",
-          })}>
-          <Tab.Screen name='Home' component={HomeScreen} />
-          <Tab.Screen name='News' component={NewsScreen} />
-          <Tab.Screen name='Weather' component={WeatherScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+            return <Feather name={iconName} size={size} color={color} />;
+          },
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        })}>
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='News' component={NewsScreen} />
+        <Tab.Screen name='Weather' component={WeatherScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
