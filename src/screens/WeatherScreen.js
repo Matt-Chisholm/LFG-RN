dc0181406de4be0e26cd42d4aa050225362bb0ad
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-16 10:08:02 -0800

    geolocation

--- src/screens/WeatherScreen.js
+++ src/screens/WeatherScreen.js
@@ -1,7 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
+import * as Location from "expo-location";
 
 export default function WeatherScreen() {
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View>
       <Text>Weather</Text>
